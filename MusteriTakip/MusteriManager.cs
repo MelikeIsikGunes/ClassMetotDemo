ff0140ccb3313d9da26f14e44d5d8ba37bb36be3
Melike Işık <melike.34.37@hotmail.com>
2021-01-17 17:52:11 +0300

    Add project files.

---
+++ MusteriTakip/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusteriTakip
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi+ " " + musteri.Soyadi +" eklendi");
+        }
+        public void MusteriListele(params Musteri[] musteriler)
+        {
+            for (int i = 0; i < musteriler.Length; i++)
+            {
+                Console.WriteLine(musteriler[i].Adi+" "+ musteriler[i].Soyadi);
+            }
+            Console.WriteLine("Müşteriler Listelendi");
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi +" silindi");
+        }
+    }
+}
---
+++ MusteriTakip/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MusteriTakip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Melike";
+            musteri1.Soyadi = "Işık Güneş";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Fatih";
+            musteri2.Soyadi = "Güneş";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Engin";
+            musteri3.Soyadi = "Demiroğ";
+
+            MusteriManager musteri = new MusteriManager();
+            musteri.MusteriEkle(musteri1);
+            musteri.MusteriEkle(musteri2);
+            musteri.MusteriEkle(musteri3);
+
+
+            Console.WriteLine("-------------------------");
+
+            musteri.MusteriListele(musteri1,musteri2,musteri3);
+
+            Console.WriteLine("-------------------------");
+
+            musteri.MusteriSil(musteri3);
+
+
+
+        }
+    }
+}
